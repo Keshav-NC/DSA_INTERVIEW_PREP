a5dd1bd74505b6b34edf1067068df86d8915aff4
Keshav <keshav.nc30@gmail.com>
2024-05-27 07:10:27 +0530

    searching updated

--- Arrays/src/com/ArraysQuestions/Recursion/Searching.java
+++ Arrays/src/com/ArraysQuestions/Recursion/Searching.java
@@ -3,22 +3,33 @@ package com.ArraysQuestions.Recursion;
 public class Searching {
     public static void main(String[] args) {
         int[] nums = {3, 2, 1, 18, 9};
-        int target = 9000;
+        int target = 9;
         System.out.println(search(nums, target));
     }
 
-    static int search (int[] nums, int target) {
-        return helper (nums, target, 0) ;
+    static boolean search (int[] nums, int target) {
+        return helperBool (nums, target, 0) ;
     }
 
-    static int helper (int[] nums, int target, int index) {
-        if (index > nums.length - 1) {
+    static boolean helperBool (int[] nums, int target, int index) {
+        if (index == nums.length) {
+            return false;
+        }
+
+        if (nums[index] == target) {
+            return true;
+        }
+        return nums[index] != target && helperBool(nums, target, ++index);
+    }
+
+    static int helperInt (int[] nums, int target, int index) {
+        if (index == nums.length) {
             return -1;
         }
 
         if (nums[index] == target) {
             return index;
         }
-        return target != nums[index] && helper(nums, target, ++index);
+        return helperInt(nums, target, ++index);
     }
 }
