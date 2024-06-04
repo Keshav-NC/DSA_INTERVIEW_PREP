73b999d82d0c763516523ffa09fa76197316c773
Keshav-NC <123293109+Keshav-NC@users.noreply.github.com>
2024-06-04 11:32:31 +0530

    Update PhonePad.java

--- DSA/src/com/keshav/Recursion/Questions/PhonePad.java
+++ DSA/src/com/keshav/Recursion/Questions/PhonePad.java
@@ -31,12 +31,12 @@ public class PhonePad {
 
         int digit = up.charAt(0) - '0';
 
-        int i = (digit - 2) * 3;
+        int from = (digit - 2) * 3;
         if(digit > 7) {
-            i+=1;
+            from+=1;
         }
 
-        int to = i + 3;
+        int to = from + 3;
 
         if(digit == 7 || digit == 9) {
             to += 1;
@@ -44,8 +44,8 @@ public class PhonePad {
 
         // local to this call
         ArrayList<String> list = new ArrayList<>();
-        for (; i < to; i++) {
-            char ch = (char) ('a' + i);
+        for (; from < to; from++) {
+            char ch = (char) ('a' + from);
             list.addAll(phoneAl(p + ch, up.substring(1)));
         }
         return list;
