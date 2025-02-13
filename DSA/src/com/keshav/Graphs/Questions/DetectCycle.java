dc4de6a3968f2d0c640d56103c004bbc670567b4
Keshav <keshav.nc30@gmail.com>
2025-02-14 01:00:58 +0530

    Update DetectCycle.java

--- DSA/src/com/keshav/Graphs/Questions/DetectCycle.java
+++ DSA/src/com/keshav/Graphs/Questions/DetectCycle.java
@@ -8,17 +8,26 @@ import java.util.Queue;
 public class DetectCycle {
     public static void main(String[] args) {
 //        [[1,2],[1,3],[2,3]]
-        List<Integer>[] adj = new List[5];
+        List<Integer>[] adj = new List[6];
         for (int i = 0; i < adj.length; i++) {
             adj[i] = new ArrayList<>();
         }
+//        adj[0].add(1);
+//        adj[0].add(2);
+//        adj[1].add(1);
+//        adj[1].add(3);
+//        adj[2].add(2);
+//        adj[2].add(3);
+
         adj[0].add(1);
-        adj[0].add(2);
-        adj[1].add(1);
-        adj[1].add(3);
-        adj[2].add(2);
+        adj[1].add(2);
         adj[2].add(3);
-        System.out.println(isCycle(adj));
+        adj[3].add(4);
+        adj[3].add(5);
+        adj[5].add(4);
+
+//        System.out.println(isCycle(adj));
+        System.out.println(bfsD(adj, 6));
     }
 
     private static boolean isCycle(List<Integer>[] adj) {
@@ -94,4 +103,31 @@ public class DetectCycle {
         pathVisited[src] = false;
         return false;
     }
+
+    private static boolean bfsD(List<Integer>[] adj, int n) {
+        boolean[] visited = new boolean[n];
+        boolean[] pathVisited = new boolean[n];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        visited[0] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            pathVisited[node] = !adj[node].isEmpty();
+
+            for (int neighbour : adj[node]) {
+                if (pathVisited[neighbour]) {
+                    return true;
+                } else if (visited[neighbour]){
+                    continue;
+                } else {
+                    queue.offer(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
